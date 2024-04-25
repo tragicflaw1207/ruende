19b6285254ce10ce7277621eebdf3301f601ad3b
Izen1 <zmfhd3789@gmail.com>
2024-04-25 23:13:23 +0900

    웅

--- src/main/java/net/ledestudio/lededev/Main.java
+++ src/main/java/net/ledestudio/lededev/Main.java
@@ -3,5 +3,6 @@ package net.ledestudio.lededev;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("웅");
     }
 }
