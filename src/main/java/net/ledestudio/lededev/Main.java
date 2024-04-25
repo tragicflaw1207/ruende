47e08bd761530bd8978fd82c615a7bf20e576573
S-E0K <rqt6238@naver.com>
2024-04-25 23:16:33 +0900

    ㅁㄴㅇㄹ

--- src/main/java/net/ledestudio/lededev/Main.java
+++ src/main/java/net/ledestudio/lededev/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("나의 흔적이야.");
+        System.out.println("히히 내꺼");
     }
 }
