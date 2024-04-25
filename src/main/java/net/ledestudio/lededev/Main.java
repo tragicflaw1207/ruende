1d4c4659e0eb6493955cf477ff32c41038df6c88
김현성 <khso6773@naver.com>
2024-04-25 23:15:23 +0900

    my own code_201955087

--- src/main/java/net/ledestudio/lededev/Main.java
+++ src/main/java/net/ledestudio/lededev/Main.java
@@ -4,4 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void exampleFor() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
 }
