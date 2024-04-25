e572d4506fda5f854bf456b604971299e68d179a
Izen1 <zmfhd3789@gmail.com>
2024-04-25 23:14:31 +0900

    웅

--- src/main/java/net/ledestudio/lededev/Main.java
+++ src/main/java/net/ledestudio/lededev/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("웅");
+        System.out.println("여긴 캬루가 점령한다");
     }
 }
